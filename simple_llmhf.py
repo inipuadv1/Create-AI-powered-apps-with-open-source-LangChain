565b3712820ec4ae882e87b9c2e008cceae26435
inipuadv1 <120273539+inipuadv1@users.noreply.github.com>
2024-03-06 05:31:11 +0000

    LLM

---
+++ simple_llmhf.py
@@ -0,0 +1,7 @@
+import os
+from langchain_community.llms import HuggingFaceEndpoint
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+llm = HuggingFaceEndpoint(repo_id="google/flan-ul2")
+text = "Beritahu fakta menarik tentang kentang!"
+result = llm.invoke(text)
+print(result)
